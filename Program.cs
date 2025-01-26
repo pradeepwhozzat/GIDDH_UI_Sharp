45c54a95c55675fb57c0872902ccd228ff8872db
Ashish Ranjan <131962116+ashish975@users.noreply.github.com>
2025-01-26 13:36:07 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
-
-builder.WebHost.ConfigureKestrel(options =>
-{
+builder.Services.AddSingleton<PdfRendererConfigService>();
+builder.WebHost.ConfigureKestrel(options => {
     options.ListenAnyIP(5000);
 });
 var app = builder.Build();
-
+// Eager initialization of the ChromePdfRenderer through PdfRendererConfigService
+var rendererConfigService = app.Services.GetRequiredService<PdfRendererConfigService>();
+rendererConfigService.GetConfiguredRenderer();
 app.MapControllers();
 app.Run();
