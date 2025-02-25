148d5706891cb9793535974a67b521f5d96571a1
Divyanshu <divyanshushrivastava123@gmail.com>
2025-02-25 16:24:59 +0530

    Remove Iron Pdf Code

--- Program.cs
+++ Program.cs
@@ -1,14 +1,8 @@
-using GiddhTemplate.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
-builder.Services.AddSingleton<PdfRendererConfigService>();
 builder.WebHost.ConfigureKestrel(options => {
     options.ListenAnyIP(5000);
 });
 var app = builder.Build();
-// Eager initialization of the ChromePdfRenderer through PdfRendererConfigService
-var rendererConfigService = app.Services.GetRequiredService<PdfRendererConfigService>();
-rendererConfigService.GetConfiguredRenderer();
 app.MapControllers();
 app.Run();
