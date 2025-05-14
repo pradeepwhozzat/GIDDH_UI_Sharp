56c0a6ac73cbaa034ba21ab7348324944107e50c
Ashish Ranjan <131962116+ashish975@users.noreply.github.com>
2025-05-14 10:13:57 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
-builder.WebHost.ConfigureKestrel(options => {
-    options.ListenAnyIP(5000);
-});
-var app = builder.Build();
-app.MapControllers();
-app.Run();
+using GiddhTemplate.Services;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllers();
+        builder.Services.AddSingleton<PdfService>();
+        builder.WebHost.ConfigureKestrel(options =>
+        {
+            options.ListenAnyIP(5000);
+        });
+        var app = builder.Build();
+        await PdfService.GetBrowserAsync();
+        app.MapControllers();
+        await app.RunAsync();
+    }
+}
